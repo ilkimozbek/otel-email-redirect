a6b977d5b4ba77e925fc4ca18520918bb5cf03b8
ilkimozbek <57453625+ilkimozbek@users.noreply.github.com>
2022-03-29 13:13:01 +0300

    Update and rename randommail.js to index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+/* server.js - Express server*/
+'use strict';
+const log = console.log
+log('Express server')
+
+const express = require('express')
+const app = express();
+
+const path = require('path');
+
+app.get('/', async function(req, res) {
+    const addresses = ["info@anemonhotels.com,reservations@baruthotels.com,info@crystalhotels.com.tr,istanbulsatis@kayahotels.com.tr,brand@rixos.com"]
+    const address_1 = addresses[Math.floor(Math.random() * addresses.length)]
+    const address_2 = 'ilkim.ozbek@kafessizturkiye.org'
+    
+
+    let mail = `mailto:${address_1}?bcc=${address_2}`
+    res.redirect(mail)
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+}) 
+
+////\r\n
