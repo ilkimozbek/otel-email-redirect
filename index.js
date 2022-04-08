f1dfd599a4c20cb0d0b4ae46aa340f2fe3e9b3b6
ilkimozbek <57453625+ilkimozbek@users.noreply.github.com>
2022-04-08 22:42:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const path = require('path');
 app.get('/', async function(req, res) {
     const addresses = ["info@anemonhotels.com","reservations@baruthotels.com","info@crystalhotels.com.tr","istanbulsatis@kayahotels.com.tr","brand@rixos.com"]
     const address_1 = addresses[Math.floor(Math.random() * addresses.length)]
-    const address_2 = 'ilkim.ozbek@kafessizturkiye.com'
+    
     
 
-    let mail = `mailto:${address_1}?bcc=${address_2}`
+    let mail = `mailto:${address_1}`
     res.redirect(mail)
 })
 
